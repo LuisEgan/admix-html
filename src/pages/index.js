937b519ad9fa6aa3bfdfbe71c6576f18635ac999
LDEV <luisdegan10@hotmail.com>
2019-04-04 20:05:23 -0300

    add alert

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react"
-// import { Link } from "gatsby"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
